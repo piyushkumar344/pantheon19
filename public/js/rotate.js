79b18f8176cbe8affa7008bceeafc615e8971496
shivansh2598 <shivanshsinha2598@gmail.com>
2019-08-10 14:46:33 +0530

    index merge with scroll

--- public/js/rotate.js
+++ public/js/rotate.js
@@ -1,5 +1,7 @@
 var c = document.getElementById('starfield');
 var n = c.getContext('2d');
+c.width = $(window).width();
+c.height = $(window).height();
 
 // View matrix, defines where you're looking
 var viewMtx = mat4.create();
@@ -36,6 +38,7 @@ function ComputeViewMtx(angle) {
     ];
 }
 
+
 projMtx = ComputeProjMtx(70, c.width / c.height, 1, 200, true);
 
 var angle = 0;
@@ -53,15 +56,15 @@ function Star() {
         this.y = Math.random() * maxDist - (maxDist / 2);
         this.z = Math.random() * maxDist - (maxDist / 2);
         var d = this.x * this.x +
-                this.y * this.y +
-                this.z * this.z;
+            this.y * this.y +
+            this.z * this.z;
     } while (
-         d > maxDist * maxDist / 4 || d < minDist * minDist
+        d > maxDist * maxDist / 4 || d < minDist * minDist
     );
     this.dist = Math.sqrt(d);
 }
 
-Star.prototype.AsVector = function() {
+Star.prototype.AsVector = function () {
     return [this.x, this.y, this.z, 1];
 }
 
@@ -71,30 +74,30 @@ for (var i = 0; i < 5000; i++) stars.push(new Star());
 var lastLoop = Date.now();
 
 function loop() {
-    
+
     var now = Date.now();
     var dt = (now - lastLoop) / 1000.0;
     lastLoop = now;
-    
+
     angle += 3.0 * dt;
 
     viewMtx = ComputeViewMtx(angle);
-    
+
     //console.log('---');
     //console.log(projMtx);
     //console.log(viewMtx);
-    
+
     mat4.multiply(viewProjMtx, projMtx, viewMtx);
     //console.log(viewProjMtx);
-    
+
     n.beginPath();
     n.rect(0, 0, c.width, c.height);
     n.closePath();
     n.fillStyle = '#14161E';
     n.fill();
-    
+
     n.fillStyle = '#fff';
-    
+
     var v = vec4.create();
     for (var i = 0; i < stars.length; i++) {
         var star = stars[i];
@@ -103,25 +106,25 @@ function loop() {
         v[1] /= v[3];
         v[2] /= v[3];
         //v[3] /= v[3];
-        
+
         if (v[3] < 0) continue;
 
         var x = (v[0] * 0.5 + 0.5) * c.width;
         var y = (v[1] * 0.5 + 0.5) * c.height;
-        
+
         // Compute a visual size...
         // This assumes all stars are the same size.
         // It also doesn't scale with canvas size well -- we'd have to take more into account.
         var s = 200 / star.dist;
-        
-        
+
+
         n.beginPath();
         n.arc(x, y, s, 0, Math.PI * 2);
         //n.rect(x, y, s, s);
         n.closePath();
         n.fill();
     }
-    
+
     window.requestAnimationFrame(loop);
 }
 
