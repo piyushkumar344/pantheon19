8a68f6826c641754d3529ad86cd10409b34dc0df
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-08-10 15:27:25 +0530

    Canvas in all section

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let port = 4000 || process.env.PORT;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running  on port ${port}`);
 });
