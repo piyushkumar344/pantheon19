3295f5cc19541c3e8f9f02b95a755006d47719dd
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2019-08-12 17:08:03 +0530

    Renamed register.js

--- models/user.js
+++ models/user.js
@@ -3,51 +3,50 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    phoneNo:{
-        type:Number
-    },
-    instituteName:{
+    email: {
         type: String,
         required: true
     },
-    instituteCity:{
+    password: {
         type: String,
         required: true
     },
-    instituteState:{
-        type: String,
-        required: true
-    },
-    instituteId:{
-        type: String,
-        required: true
-    },
-    emailOTP:{
+    phoneNo: {
         type: Number
     },
-    phoneOTP:{
+    instituteName: {
+        type: String,
+        required: true
+    },
+    instituteCity: {
+        type: String,
+        required: true
+    },
+    instituteState: {
+        type: String,
+        required: true
+    },
+    instituteId: {
+        type: String,
+        required: true
+    },
+    emailOTP: {
         type: Number
     },
-    isVerified:{
+    phoneOTP: {
+        type: Number
+    },
+    isVerified: {
         type: Boolean
     },
-    hasAllFields:{
+    hasAllFields: {
         type: Boolean
     },
-    pantheonId:{
+    pantheonId: {
         type: String
     }
 });
-  
-  const User = mongoose.model('users', userSchema);
-  module.exports=User;
 
-  
+const User = mongoose.model('users', userSchema);
+module.exports = User;
+
--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('../config');
-const userData = require('../models/register');
+const userData = require('../models/user');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 
