ec0cd51d44479f05e13207ca679a298c3e57a56b
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2019-09-04 00:06:29 +0530

    Added route to get user details

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,30 @@
 const express = require('express');
-const router = express.Router();
 const bcrypt = require('bcryptjs');
-const config = require('../config');
-const userData = require('../models/user');
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+const UserModel = require('../models/user');
+const TeamModel = require('../models/team');
+const EventModel = require('../models/event');
+const PanIdCounter = require('../models/panIdCounter');
+const TeamIdCounter = require('../models/teamIdCounter');
+const verifyToken = require('../middlewares/verifyToken');
+const router = express.Router();
+
+router.get('/user',verifyToken,(req,res)=>{
+    const userId = req.userId;
+    async function getUser(){
+        try{
+            await UserModel.findById(userId,(err,user)=>{
+                if(err) res.send("Could not fetch required details!");
+                else if(!user) res.send("Could not find user!");
+                else{
+                    res.send(user);
+                }
+            })
+        }
+        catch(e){
+            res.send(e.message);
+        }
+    }
+    getUser();
+});
