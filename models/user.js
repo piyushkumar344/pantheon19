dce767dba4d123372b850b4a1eebb7f8f60ae0ed
shivansh2598 <shivanshsinha2598@gmail.com>
2019-09-04 00:07:43 +0530

    profile

--- models/user.js
+++ models/user.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
+    name: {
+        type:String,
+        required: true
+    },
     email: {
         type: String,
         required: true
