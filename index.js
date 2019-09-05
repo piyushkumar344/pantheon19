b4624f5ce49bb32bb7de9628292260de9b2ec2ca
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-09-05 18:26:27 +0530

    Index page updated

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/view', 'index.html'));
 });
 
-
 //port connection
 let port = 4000 || process.env.PORT;
 app.listen(port, () => {
