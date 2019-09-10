f1314b5f006b29c863dc24b2bb09e932b9b924bd
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-09-10 10:48:06 +0530

    removed / route

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use('/event', event);
 app.use('/', feedback);
 
 //routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.get('*', (req, res) => {
-    res.send("Go back");
-})
+// app.get('*', (req, res) => {
+//     res.send("Go back");
+// })
 
 //port connection
 let port = 4000 || process.env.PORT;
