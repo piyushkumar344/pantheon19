a391eef2e08884c606d6d00bbaab6f980ca419d1
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-09-10 10:44:37 +0530

    Deleted fronted

--- public/js/preloader.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    'use strict';
-    $(window).on('load', function () {
-        setTimeout(function () {
-            var preLoder = $("#preloader");
-            preLoder.delay(200).fadeOut(500);
-        }, 100);
-    });
-})(jQuery)
