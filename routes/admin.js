c855bf9b4219875d38635bfc36c227f53862459b
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-09-25 22:38:51 +0530

    Added admin routes raw

---
+++ routes/admin.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/teamDetails', (req, res) => {
+
+});
+
+router.post('/verifyTeam', (req, res) => {
+
+});
+
+router.post('/deverifyTeam', (req, res) => {
+
+});
+
+router.get('/leaderboard', (req, res) => {
+
+});
+
+router.post('/eventWinners', (req, res) => {
+
+});
+
+router.post('/notification', (req, res) => {
+
+});
+
+router.get('/notifications', (req, res) => {
+
+});
+
+router.post('/updateEvents', (req, res) => {
+
+});
+
+router.post('/userRegisterAdmin', (req, res) => {
+
+});
+
+router.post('/teamRegisterAdmin', (req, res) => {
+
+});
