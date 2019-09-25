dace39b1f6f4cd5d0a80822c909a32c91acf6aea
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2019-09-25 23:29:16 +0530

    Verfiy and reject routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,33 @@ router.get('/teamDetails', (req, res) => {
 });
 
 router.post('/verifyTeam', (req, res) => {
-
+    const id = req.teamId;
+    async function teamVerify() {
+        try {
+            let team = await TeamModel.findOne({ teamId: id });
+            team.teamVerified = true;
+            let teamUpdate = team.save();
+            return res.json({ status: 200, message: "Team Verified Successfully" });
+        } catch (e) {
+            return res.json({ status: 500, message: "Error on the server!" });
+        }
+    }
+    teamVerify();
 });
 
 router.post('/deverifyTeam', (req, res) => {
-
+    const id = req.teamId;
+    async function teamReject() {
+        try {
+            let team = await TeamModel.findOne({ teamId: id });
+            team.teamVerified = false;
+            let teamUpdate = team.save();
+            return res.json({ status: 200, message: "Team Verified Successfully" });
+        } catch (e) {
+            return res.json({ status: 500, message: "Error on the server!" });
+        }
+    }
+    teamReject();
 });
 
 router.get('/leaderboard', (req, res) => {
